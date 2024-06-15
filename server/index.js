2697bb3e821e6a4ee4c0e9e54a91dbcc6f8eff7d
Siddharth Pilkhn <sidpel03@gmail.com>
2024-06-15 20:51:58 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -23,10 +23,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin:"http://localhost:3000",
+        origin:"*",
         credentials:true,
     })
 )
+// http://localhost:3000
 
 app.use(
     fileUpload({
