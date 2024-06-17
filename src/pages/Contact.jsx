482214a4a185872eca483f4bfad0778c98717caa
siddharthpelkhn <109087112+siddharthpelkhn@users.noreply.github.com>
2024-06-17 14:33:06 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,7 @@ import React from "react"
 import Footer from "../components/Common/Footer"
 import ContactDetails from "../components/ContactPage/ContactDetails"
 import ContactForm from "../components/ContactPage/ContactForm"
+import ReviewSlider from "../components/Common/ReviewSlider"
 
 const Contact = () => {
   return (
